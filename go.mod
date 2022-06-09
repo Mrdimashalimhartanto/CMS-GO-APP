8c55dfeab676a7f1b40166e0b1e3de1b517d63a1
Mrdimashalimhartanto <dimashalimgit@gmail.com>
2022-06-09 12:02:32 +0700

    cms-go-app get module repository

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Mrdimashalimhartanto/CMS-APP-GO
+
+go 1.18
+
+require github.com/Mrdimashalimhartanto/CMS-GO v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	cms_go "github.com/Mrdimashalimhartanto/CMS-GO"
+)
+
+func main() {
+	//cms_go.SayHelloCms()
+
+	fmt.Println(cms_go.SayHelloCms())
+}
